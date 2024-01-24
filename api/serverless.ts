236bfd08874414d94f1f85d2bb8b9b03abb6c529
Vishal Kumar <25267239+vishalx360@users.noreply.github.com>
2024-01-24 21:43:08 +0530

    chore: test config fix

--- api/serverless.ts
+++ api/serverless.ts
@@ -13,7 +13,7 @@ const app = Fastify({
 });
 
 // Register your application as a normal plugin.
-app.register(import("../src/app"));
+app.register(require("../src/app"));
 
 export default async (req, res) => {
   await app.ready();
