c27c49627e52e5d17071363e5ef7d9689cf74401
Vishal Kumar <25267239+vishalx360@users.noreply.github.com>
2024-01-24 21:02:32 +0530

    chore: use env PORT

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import fastify from "./app";
 import { env } from "./env";
 
-const FASTIFY_PORT = Number(env.FASTIFY_PORT) || 3000;
+const PORT = Number(env.PORT) || 3000;
 
-fastify.listen({ port: FASTIFY_PORT });
+fastify.listen({ port: PORT });
 
-console.log(`🚀  Fastify server running on port http://localhost:${FASTIFY_PORT}`);
+console.log(`🚀  Fastify server running on port http://localhost:${PORT}`);
 console.log(`Route index: /`);
 console.log(`Route auth: /api/auth`);
 console.log(`Route task: /api/task`);
